a67e4f08c68522f47f598205f531d601e799bd65
Naoya Ozaki <naoya.ozaki11@gmail.com>
2022-07-10 17:38:37 +0900

    x2tofという関数をPykep C++のLambert's Problemを元に書き直した

---
+++ notebooks/src/lambert_problem.jl
@@ -0,0 +1,15 @@
+""" Lambert's Problem Solver in Julia
+"""
+# ----------------------------------------------------------------
+__author__ = "Naoya Ozaki"
+__affiliation__ = "Institute of Space and Astronautical Science, \
+                   Japan Aerospace Exploration Agency"
+__email__ = "ozaki.naoya@jaxa.jp"
+__date__ = "10 July 2022"
+__version__ = "0.0.1"
+__status__ = "Development"
+__copyright__ = "Copyright 2022, nomad project"
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+
+
